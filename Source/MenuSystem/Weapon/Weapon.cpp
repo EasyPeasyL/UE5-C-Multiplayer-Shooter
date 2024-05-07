835d78eeb8819431ce72fb5a4a9da9b1666964dc
이정민 <qowwl@naver.com>
2024-05-07 12:17:43 +0900

    71. Fire Weapon Effects

--- Source/MenuSystem/Weapon/Weapon.cpp
+++ Source/MenuSystem/Weapon/Weapon.cpp
@@ -6,6 +6,8 @@
 #include "Components/WidgetComponent.h"
 #include "MenuSystem/Character/BlasterCharacter.h"
 #include "Net/UnrealNetwork.h"
+#include "Animation/AnimationAsset.h"
+#include "Components/SkeletalMeshComponent.h"
 
 // Sets default values
 AWeapon::AWeapon()
@@ -104,5 +106,13 @@ void AWeapon::ShowPickupWidget(bool bShowWidget)
 	if (PickupWidget)
 	{
 		PickupWidget->SetVisibility(bShowWidget);
+	}
+}
+
+void AWeapon::Fire()
+{
+	if (FireAnimation)
+	{
+		WeaponMesh->PlayAnimation(FireAnimation, false);
 	}
 }
