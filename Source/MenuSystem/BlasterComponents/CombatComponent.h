a669ff0daed1219290121dc21303dea5dce4b044
DESKTOP-BNTAOND\EasyPeasyL <qowwl@naver.com>
2025-01-18 17:31:48 +0900

    114. Carried Ammo

--- Source/MenuSystem/BlasterComponents/CombatComponent.h
+++ Source/MenuSystem/BlasterComponents/CombatComponent.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "MenuSystem/HUD/BlasterHUD.h"
+#include "MenuSystem/Weapon/WeaponTypes.h"
 #include "CombatComponent.generated.h"
 
 #define TRACE_LENGTH 80000.f
@@ -108,6 +109,15 @@ private:
 	void FireTimerFinished();
 
 	bool CanFire();
+
+	// Carried ammo for the currently-equipped weapon
+	UPROPERTY(ReplicatedUsing = OnRep_CarriedAmmo)
+	int32 CarriedAmmo;
+
+	UFUNCTION()
+	void OnRep_CarriedAmmo();
+
+	TMap<EWeaponType, int32> CarriedAmmoMap;
 
 public:
 		
