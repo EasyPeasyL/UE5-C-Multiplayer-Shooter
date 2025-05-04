7ec927c3ad4ef86909b65058b1ad6af85d05231a
DESKTOP-BNTAOND\EasyPeasyL <qowwl@naver.com>
2025-05-04 21:34:37 +0900

    162. Health Pickup

---
+++ Source/MenuSystem/Pickups/HealthPickup.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPickup.h"
+#include "MenuSystem/Character/BlasterCharacter.h"
+#include "MenuSystem/BlasterComponents/BuffComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+
+AHealthPickup::AHealthPickup()
+{
+    bReplicates = true;
+    PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+    PickupEffectComponent->SetupAttachment(RootComponent);
+}
+
+void AHealthPickup::Destroyed()
+{
+    if (PickupEffect)
+    {
+        UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+            this,
+            PickupEffect,
+            GetActorLocation(),
+            GetActorRotation()
+        );
+    }
+    Super::Destroyed();
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+    ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+    if (BlasterCharacter)
+    {
+
+    }
+
+    Destroy();
+}
