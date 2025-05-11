22f78d4b86fde818cf9fa001db29be830ac9800b
DESKTOP-BNTAOND\EasyPeasyL <qowwl@naver.com>
2025-05-11 14:01:11 +0900

    180. Replicating Scatter

--- Source/MenuSystem/Weapon/HitScanWeapon.h
+++ Source/MenuSystem/Weapon/HitScanWeapon.h
@@ -18,8 +18,6 @@ public:
 
 protected:
 
-    FVector TraceEndWithScatter(const FVector& TraceStart, const FVector& HitTarget);
-
     void WeaponTraceHit(const FVector& TraceStart, const FVector& HitTarget, FHitResult& OutHit);
 
     UPROPERTY(EditAnywhere)
@@ -41,18 +39,5 @@ private:
 
     UPROPERTY(EditAnywhere)
     USoundCue* FireSound;
-
-    /**
-    * Trace end with scatter
-    */
-
-    UPROPERTY(EditAnywhere, Category = "Weapon Scatter")
-    float DistanceToSphere = 800.f;
-
-    UPROPERTY(EditAnywhere, Category = "Weapon Scatter")
-    float SphereRadius = 75.f;
-
-    UPROPERTY(EditAnywhere, Category = "Weapon Scatter")
-    bool bUseScatter = false;
 
 };
