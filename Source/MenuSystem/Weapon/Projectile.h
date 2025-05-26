2f26fffccf77183647f5d601ad65497ecd83df38
DESKTOP-BNTAOND\EasyPeasyL <qowwl@naver.com>
2025-05-26 11:53:42 +0900

    203. Spawning Projectiles Locally

--- Source/MenuSystem/Weapon/Projectile.h
+++ Source/MenuSystem/Weapon/Projectile.h
@@ -27,6 +27,9 @@ public:
 	UPROPERTY(EditAnywhere)
 	float InitialSpeed = 15000;
 
+	UPROPERTY(EditAnywhere)
+	float Damage = 20.f;
+
 protected:
 	virtual void BeginPlay() override;
 	void StartDestroyTimer();
@@ -36,9 +39,6 @@ protected:
 
 	UFUNCTION()
 	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
-
-	UPROPERTY(EditAnywhere)
-	float Damage = 20.f;
 
 	UPROPERTY(EditAnywhere)
 	class UParticleSystem* ImpactParticles;
